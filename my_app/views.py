fe196696f3bc9bd869bb33c83a1d65982bc57c0d
Drumknott <86430425+Drumknott@users.noreply.github.com>
2023-01-02 13:48:53 +0000

    Got HTML pages working

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
+
+# def index(request):
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render())
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'index.html')
