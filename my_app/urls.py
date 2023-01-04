d12583cdddfb97619a83a77fab3ed8d507c9e2d6
Drumknott <86430425+Drumknott@users.noreply.github.com>
2023-01-04 13:39:49 +0000

    Index page and two linked html pages working, with css.
    Fuck Django.

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('index.html', views.index),
     path('products.html', views.products),
     path('aboutMe.html', views.about_me)
 ]
